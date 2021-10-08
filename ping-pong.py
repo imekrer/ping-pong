80be767ec9135fbb16052da87eea0e7f1707246f
imekrer <90513415+imekrer@users.noreply.github.com>
2021-10-08 18:27:05 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -13,6 +13,9 @@ speed_y=3
 X=350
 Y=250
 
+Y1=0
+Y2=680
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -21,13 +24,19 @@ while game:
     clock= time.Clock()
     FPS=60
     clock.tick(FPS)
-    #circle moving
-    draw.circle(window, white, (X, Y),40)
-    draw.rect(window, white, (200,200),(100,100))
+    draw.circle(window, white, (X, Y),30)
+    draw.rect(window, white, (Y1, 150, 15, 150))
+    draw.rect(window, white, (Y2, 150, 15, 150))
     X+=speed_x
     Y+=speed_y
     print(speed_y)
     display.update()
+    #rect moving
+    keys_pressed= key.get_pressed()
+    if keys_pressed[K_w]:
+        Y1=-3
+
+    #circle moving
     if Y>450:
         speed_y=-3
     if Y<50:
